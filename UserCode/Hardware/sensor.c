31fc1bcbb898c9d0e1e012af92c27af35293a355
MaiXiangBao <SweetWheatBread@gmail.com>
2026-01-24 16:15:53 +0800

    sensor.h & sensor_pid.h 循迹PID完成

---
+++ UserCode/Hardware/sensor.c
@@ -0,0 +1,62 @@
+/************************************************************************************************
+* @name   红外循迹与误差获取模块
+* @note   PID计算不在这里！
+* @author MxBz_
+*************************************************************************************************/
+
+#include "zf_common_headfile.h"
+#include "sensor.h"
+
+/* ==============================================================================================
+                                        全局变量定义
+   ============================================================================================== */
+// 为之后和菜单打配合准备，本来是要直接设成宏定义的
+int16_t L4_WEIGHT = 8;           // 外面灯的权重
+int16_t L3_WEIGHT = 5;           // 次外面灯的权重
+int16_t L2_WEIGHT = 3;           // 次内部灯的权重
+int16_t L1_WEIGHT = 1;           // 内部灯的权重
+/* ==============================================================================================
+                                        函数定义
+   ============================================================================================== */
+
+/** 
+ * @brief 传感器初始化
+ * @note 一些简单的gpio_init堆砌
+ * @return
+ */
+void Sensor_Init(void)
+{
+    gpio_init(E8, GPI, GPIO_LOW, GPI_PULL_DOWN);
+    gpio_init(E9, GPI, GPIO_LOW, GPI_PULL_DOWN);
+    gpio_init(E10, GPI, GPIO_LOW, GPI_PULL_DOWN);
+    gpio_init(E11, GPI, GPIO_LOW, GPI_PULL_DOWN);
+    gpio_init(E12, GPI, GPIO_LOW, GPI_PULL_DOWN);
+    gpio_init(E13, GPI, GPIO_LOW, GPI_PULL_DOWN);
+    gpio_init(E14, GPI, GPIO_LOW, GPI_PULL_DOWN);
+    gpio_init(E15, GPI, GPIO_LOW, GPI_PULL_DOWN);
+    
+}
+
+/** 
+ * @brief 获取 Error 值
+ * @note 给每个灯一个权重，然后加和，等下传到sensor_pid去
+ * @return 一个 `int16_t` 值，代表权重后的Error
+ */
+int16_t Sensor_GetError(void)
+{
+    int16_t Error = 0;
+    // LEFT
+    Error -= ( L4_WEIGHT * Left4
+             + L3_WEIGHT * Left3
+             + L2_WEIGHT * Left2
+             + L1_WEIGHT * Left1);
+    // RIGHT
+    Error += ( L4_WEIGHT * Right4
+             + L3_WEIGHT * Right3
+             + L2_WEIGHT * Right2
+             + L1_WEIGHT * Right1);
+    
+    return Error;
+}
+
+

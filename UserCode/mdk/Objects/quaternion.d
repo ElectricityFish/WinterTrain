7b435b5ead92fdd76a0b7c99352f0b00b16551ef
lawlite_wei <lwqxbq@qq.com>
2026-01-27 11:37:51 +0800

    github提交遇到点问题，重新提交一下

---
+++ UserCode/mdk/Objects/quaternion.d
@@ -0,0 +1,10 @@
+./objects/quaternion.o: ..\System\quaternion.c \
+  ..\..\libraries\zf_common\zf_common_typedef.h \
+  D:\Keil_v5\ARM\ARMCLANG\include\stdio.h \
+  D:\Keil_v5\ARM\ARMCLANG\include\stdint.h \
+  D:\Keil_v5\ARM\ARMCLANG\include\stdbool.h \
+  D:\Keil_v5\ARM\ARMCLANG\include\stdarg.h \
+  D:\Keil_v5\ARM\ARMCLANG\include\string.h \
+  D:\Keil_v5\ARM\ARMCLANG\include\stdlib.h \
+  ..\..\libraries\zf_device\zf_device_mpu6050.h ..\System\quaternion.h \
+  D:\Keil_v5\ARM\ARMCLANG\include\math.h
